4afb1eaaf71c81e69de25ff2fb3e399f79abeead
Admin <admin@Admins-MacBook-Pro.local>
2024-09-19 06:03:48 +0530

    initial commit

---
+++ eureka-server/src/main/java/com/example/configuration/EurekaServerConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.configuration;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaServerConfiguration {
+
+}
